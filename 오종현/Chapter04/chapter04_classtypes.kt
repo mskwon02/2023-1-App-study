f4917c77c5ad1608239441ab20939005c87d67c5
toass <57165479+toass@users.noreply.github.com>
2023-01-17 21:29:16 +0900

    Update chapter04_classtypes.kt

--- 오종현/Chapter04/chapter04_classtypes.kt
+++ 오종현/Chapter04/chapter04_classtypes.kt
@@ -1,1 +1,17 @@
-tester
+package chapter04_classtypes
+
+
+//클래스를 선언하고 클래스의 이름으로 바로 접근하려면 companion object 키워드로 선언해야한다.
+//이때 object 클래스는 익명 클래스로 이름이 없으므로 클래스를 선언하면서 동시에 객체를 생성해야한다.
+class MyClass{
+    companion object{
+        var data = 10
+        fun someFun(){
+            println(data)
+        }
+    }
+}
+fun main(){
+    MyClass.data = 20
+    MyClass.some()
+}
